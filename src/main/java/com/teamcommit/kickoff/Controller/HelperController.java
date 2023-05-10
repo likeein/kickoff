f4786742ea904a885b9768eb994e62c4e29e4793
DongHyun <ehdgus6709@naver.com>
2023-05-10 16:55:58 +0900

    gradle

--- src/main/java/com/teamcommit/kickoff/Controller/HelperController.java
+++ src/main/java/com/teamcommit/kickoff/Controller/HelperController.java
@@ -1,5 +1,18 @@
 package com.teamcommit.kickoff.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelperController {
+
+    @GetMapping("/helperList")
+    public String helperList() {
+        String view = "/helperList";
+
+        return view;
+    }
+
+
 
 }
