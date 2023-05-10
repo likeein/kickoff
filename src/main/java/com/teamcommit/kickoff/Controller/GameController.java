15dc26342d0c1603c6db3e6f5bb4ab1dacb51ca8
cutePark <cutepark10@naver.com>
2023-05-10 15:04:43 +0900

    intellij4

--- src/main/java/com/teamcommit/kickoff/Controller/GameController.java
+++ src/main/java/com/teamcommit/kickoff/Controller/GameController.java
@@ -1,5 +1,28 @@
 package com.teamcommit.kickoff.Controller;
 
+import com.teamcommit.kickoff.Common.CommandMap;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+
 public class GameController {
 
+    @GetMapping("/game")
+    public String reservationList() {
+        String view = "/game/game";
+
+        return view;
+    }
+
+    @GetMapping("/gameUpdate")
+    public String reservationInsert() {
+        String view = "/game/gameUpdate";
+
+        return view;
+    }
 }
