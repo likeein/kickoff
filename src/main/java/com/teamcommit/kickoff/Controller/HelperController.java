2bb68bb4cf4601ed3b1e4e4d0d71ab80d7ff4e17
DongHyun <ehdgus6709@naver.com>
2023-05-11 15:24:06 +0900

    helper insert

--- src/main/java/com/teamcommit/kickoff/Controller/HelperController.java
+++ src/main/java/com/teamcommit/kickoff/Controller/HelperController.java
@@ -1,16 +1,41 @@
 package com.teamcommit.kickoff.Controller;
 
+import com.teamcommit.kickoff.Common.CommandMap;
+import com.teamcommit.kickoff.Do.HelperDO;
+import com.teamcommit.kickoff.Service.HelperService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.*;
 
 @Controller
 public class HelperController {
 
-    @GetMapping("/helperList")
-    public String helperList() {
-        String view = "/helper/helperList";
+    @Resource(name="helperService")
+    private HelperService helperService;
 
-        return view;
+    @GetMapping("/helperList")
+    public String helperList(Model model) {
+        String result = "";
+
+        try {
+            List<HelperDO> list = helperService.selectHelper();
+            for (HelperDO helper : list) {
+                result += "<tr><td>" + helper.getHelperMatch() + "</td><td>" + helper.getHelperPosition() +
+                        "</td><td>" + helper.getHelperTeamLevel() + "</td><td>" + helper.getHelperGender() +
+                        "</td><td>" + helper.getHelperPlace() + "</td><td>" + helper.getHelperTime() + "</td></tr>";
+            }
+            model.addAttribute("table", result);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "/helper/helperList";
     }
 
     @GetMapping("/helperInsert")
@@ -19,5 +44,6 @@ public class HelperController {
 
         return view;
     }
+
 
 }
--- src/main/java/com/teamcommit/kickoff/Dao/HelperDAO.java
+++ src/main/java/com/teamcommit/kickoff/Dao/HelperDAO.java
@@ -1,5 +1,30 @@
 package com.teamcommit.kickoff.Dao;
 
-public class HelperDAO {
+import com.teamcommit.kickoff.Common.AbstractDAO;
+import com.teamcommit.kickoff.Do.HelperDO;
+import com.teamcommit.kickoff.Do.HelperInsertDO;
+import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
+@Repository("helperDAO")
+public class HelperDAO extends AbstractDAO {
+
+    @SuppressWarnings("unchecked")
+    public List<HelperDO> selectHelper() throws Exception {
+        return (List<HelperDO>) selectList("helper.selectHelper");
+    }
+
+    public void insertHelper(HelperInsertDO helperInsertDO) throws Exception {
+        insert("helper.insertHelper", helperInsertDO);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> selectHelperDetail(Map<String, Object> map) throws Exception {
+        return (Map<String, Object>) selectOne("helper.selectHelperDetail", map);
+    }
+
+    public void deleteHelper(HelperDO helperDO) throws Exception {
+        delete("helper.deleteHelper", helperDO);
+    }
 }
