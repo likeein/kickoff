9fa8852b51b7a2c9b02f032ec5c14e8069eb4ab6
Jaeeun <ja2eni@naver.com>
2023-05-11 16:31:32 +0900

    reser, game

--- src/main/java/com/teamcommit/kickoff/Service/ReservationService.java
+++ src/main/java/com/teamcommit/kickoff/Service/ReservationService.java
@@ -7,7 +7,7 @@ public interface ReservationService {
 
     void insertReservation(Map<String, Object> map, HttpServletRequest request) throws Exception;
 
-    Map<String, Object> selectReservation(Map<String, Object> map) throws Exception;
+    Map<String, Object> selectReservationDetail(Map<String, Object> map) throws Exception;
 
     /*Map<String, Object> selectMemInfo(Map<String, Object> map) throws Exception;*/
 
