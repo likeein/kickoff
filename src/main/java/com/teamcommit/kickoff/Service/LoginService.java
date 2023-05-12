c28cfbea8e03c40aa905f52c12bd7901c029896e
yeonu <yyeonunu@gmail.com>
2023-05-12 13:36:15 +0900

    업체로그인

--- src/main/java/com/teamcommit/kickoff/Service/LoginService.java
+++ src/main/java/com/teamcommit/kickoff/Service/LoginService.java
@@ -1,9 +1,18 @@
 package com.teamcommit.kickoff.Service;
 
+import com.teamcommit.kickoff.Do.EmployerDO;
 import com.teamcommit.kickoff.Do.UserDO;
 
 import java.util.HashMap;
 
 public interface LoginService {
+
+    // 개인 회원 로그인
     UserDO member_login(UserDO userDO);
+
+    // 업체 회원 로그인
+    EmployerDO emp_login(EmployerDO empDO);
+
+    // 개인 회원 아이디 찾기
+    String findUser_id(String userName, String userPhoneNumber);
 }
